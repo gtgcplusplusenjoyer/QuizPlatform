9d3f17df926d6ea5a8ed029a1126b5df1a32dc23
AlexFedorets <sakhafedorets904@gmail.com>
2026-07-02 15:47:30 +0300

    feat: add refresh token configuration

---
+++ QuizPlatform.API/QuizPlatform.Infrastructure/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuizPlatform.Core.Tokens;
+
+namespace QuizPlatform.Infrastructure.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Token)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(t => t.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(t => t.ExpiresAt)
+                .IsRequired();
+
+            builder.Property(t => t.UserId)
+                .IsRequired();
+
+            builder.ToTable("RefreshTokens");
+        }
+    }
+}
--- QuizPlatform.API/QuizPlatform.Infrastructure/Context/QuizPlatformDbContext.cs
+++ QuizPlatform.API/QuizPlatform.Infrastructure/Context/QuizPlatformDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuizPlatform.Core.Entities;
+using QuizPlatform.Core.Tokens;
 using QuizPlatform.Infrastructure.Configurations;
 
 namespace QuizPlatform.Infrastructure.Context
@@ -7,12 +8,15 @@ namespace QuizPlatform.Infrastructure.Context
     public class QuizPlatformDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
         public QuizPlatformDbContext(DbContextOptions<QuizPlatformDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RefreshTokenConfiguration());
         }
 
     }
