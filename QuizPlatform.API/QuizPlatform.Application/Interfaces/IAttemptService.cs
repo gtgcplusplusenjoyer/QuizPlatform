eb2a454db915bb1d2d6a132629764ba435755353
AlexFedorets <sakhafedorets904@gmail.com>
2026-07-06 23:37:23 +0300

    fix: update dtos and service interfaces

--- QuizPlatform.API/QuizPlatform.Application/Interfaces/IAttemptService.cs
+++ QuizPlatform.API/QuizPlatform.Application/Interfaces/IAttemptService.cs
@@ -1,14 +1,14 @@
-﻿using QuizPlatform.Core.Entities;
+﻿using QuizPlatform.Application.Dto.Attempt;
 
 namespace QuizPlatform.Application.Interfaces
 {
     public interface IAttemptService
     {
-        Task CreateAttemptAsync(Guid quizId, Guid userId, CancellationToken cancellationToken);
-        Task<QuizAttempt> SaveAnswersAsync(Guid attemptId, Dictionary<Guid, string> answers, CancellationToken cancellationToken);
-        Task<QuizAttempt> FinishAttemptAsync(Guid attemptId, CancellationToken cancellationToken);  
-        Task<QuizAttempt> GetAttemptResultAsync(Guid attemptId, CancellationToken cancellationToken);
-        Task<List<QuizAttempt>> GetUserAttemptsByIdAsync(Guid userId, CancellationToken cancellationToken);
-        Task<List<QuizAttempt>> GetQuizAttemptsByIdAsync(Guid quizId,  CancellationToken cancellationToken);
+        Task<AttemptResponseDto> CreateAttemptAsync(CreateAttemptRequestDto createAttemptRequestDto, Guid userId, CancellationToken cancellationToken);
+        Task<AttemptResponseDto> SaveAnswersAsync(Guid attemptId, SubmitAnswerRequestDto submitAnswerRequestDto, CancellationToken cancellationToken);
+        Task<AttemptResultResponseDto> FinishAttemptAsync(Guid attemptId, CancellationToken cancellationToken);
+        Task<AttemptResultResponseDto> GetAttemptResultAsync(Guid attemptId, CancellationToken cancellationToken);
+        Task<List<AttemptResultResponseDto>> GetUserAttemptsByIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<List<AttemptResponseDto>> GetQuizAttemptsByIdAsync(Guid quizId, CancellationToken cancellationToken);
     }
 }
--- QuizPlatform.API/QuizPlatform.Application/Interfaces/IQuizService.cs
+++ QuizPlatform.API/QuizPlatform.Application/Interfaces/IQuizService.cs
@@ -1,14 +1,14 @@
-﻿using QuizPlatform.Core.Entities;
+﻿using QuizPlatform.Application.Dto.Quiz;
 
 namespace QuizPlatform.Application.Interfaces
 {
     public interface IQuizService
     {
-        Task<Quiz> GetQuizByIdAsync(Guid quizId,CancellationToken cancellationToken);
-        Task<List<Quiz>> GetQuizzesByAuthorIdAsync(Guid authorId, CancellationToken cancellationToken);
-        Task CreateQuizAsync(Quiz quiz,CancellationToken cancellationToken);
-        Task DeleteQuizAsync(Guid quizId, CancellationToken cancellationToken);   
-        Task UpdateQuizAsync(Guid quizId, CancellationToken cancellationToken);
+        Task<QuizResponseDto> GetQuizByIdAsync(Guid quizId, CancellationToken cancellationToken);
+        Task<List<QuizSummaryDto>> GetQuizzesByAuthorIdAsync(Guid authorId, CancellationToken cancellationToken);
+        Task<QuizResponseDto> CreateQuizAsync(CreateQuizRequestDto createQuizRequestDto, Guid userId, CancellationToken cancellationToken);
+        Task DeleteQuizAsync(Guid quizId, Guid userId, CancellationToken cancellationToken);
+        Task UpdateQuizAsync(Guid quizId, UpdateQuizRequestDto updateQuizRequestDto, Guid userId, CancellationToken cancellationToken);
         Task PublishQuizAsync(Guid quizId, CancellationToken cancellationToken);
     }
 }
